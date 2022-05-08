182672edec7df1bb80c57a8d5f3e579e1211f541
Aijie Shu <s524256521@gmail.com>
2022-05-08 21:58:22 +0800

    Initial Commit

---
+++ Feunote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Feunote
+//
+//  Created by Losd wind on 2022/5/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Feunote/FeunoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeunoteApp.swift
+//  Feunote
+//
+//  Created by Losd wind on 2022/5/8.
+//
+
+import SwiftUI
+
+@main
+struct FeunoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
