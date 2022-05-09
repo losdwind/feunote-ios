6297ca8f3cb4c20b885615c16f2d8ad237b874ad
Aijie Shu <s524256521@gmail.com>
2022-05-09 17:59:09 +0800

    setup amplify backend

--- Feunote/FeunoteApp.swift
+++ Feunote/FeunoteApp.swift
@@ -6,12 +6,31 @@
 //
 
 import SwiftUI
-
+import Amplify
+import AmplifyPlugins
 @main
 struct FeunoteApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        do {
+            Amplify.Logging.logLevel = .verbose
+            let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels())
+            try Amplify.add(plugin: dataStorePlugin)
+            try Amplify.configure()
+            print("Amplify configured with DataStore plugin")
+        } catch {
+            print("Failed to initialize Amplify with \(error)")
+        }
+        return true
     }
 }
