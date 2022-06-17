bf033368edf1ba4f6782c8065a8f2a484463d4c9
Aijie Shu <s524256521@gmail.com>
2022-06-17 09:27:02 +0800

    Change( publisher to async await)

--- Feunote/Data/Repository/StorageRepositoryImpl.swift
+++ Feunote/Data/Repository/StorageRepositoryImpl.swift
@@ -6,27 +6,71 @@
 //
 
 import Foundation
+import Combine
 import Amplify
 import AmplifyPlugins
 
 class StorageRepositoryImpl:StorageRepositoryProtocol{
     private let storageService:StorageServiceProtocol
-    
+    private var subscribers = Set<AnyCancellable>()
+
     init(storageService:StorageServiceProtocol){
         self.storageService = storageService
     }
     
-    func uploadImage(key: String, data: Data) -> StorageUploadDataOperation {
-        storageService.uploadImage(key: key, data: data)
+    func uploadImage(key: String, data: Data) async await -> String {
+        return try await withCheckedThrowingContinuation({ continuation in
+            let ops = storageService.uploadImage(key: key, data: data)
+            ops.resultPublisher.sink{receiveCompletion:
+                                        if let error {
+                                            continuation.resume(throwing: AppError.failedToLoadResource)
+                                        } receiveValue: {
+                                            key in
+                                            continuation.resume(returning:key)
+                                        }
+        }
+            .store(in:&subscribers)
+            
+
+        })
+        
     }
     
-    func downloadImage(key: String) -> StorageDownloadDataOperation {
-        storageService.downloadImage(key: key)
+    func downloadImage(key: String) async throws -> Data {
+        return try await withCheckedThrowingContinuation({ continuation in
+            let ops = storageService.downloadImage(key: key)
+            ops.resultPublisher.sink{receiveCompletion:
+                                        if let error {
+                                            continuation.resume(throwing: AppError.failedToLoadResource)
+                                        } receiveValue: {
+                                            data in
+                                            continuation.resume(returning:data)
+                                        }
+        }
+            .store(in:&subscribers)
+            
+
+        })
+        
     }
     
-    func removeImage(key: String) -> StorageRemoveOperation {
-        storageService.removeImage(key: key)
+    func removeImage(key: String) async throws-> String {
+        return try await withCheckedThrowingContinuation({ continuation in
+            let ops = storageService.removeImage(key: key)
+            ops.resultPublisher.sink{receiveCompletion:
+                                        if let error {
+                                            continuation.resume(throwing: AppError.failedToDeleteResource)
+                                        } receiveValue: {
+                                            key in
+                                            continuation.resume(returning:key)
+                                        }
+        }
+            .store(in:&subscribers)
+            
+
+        })
     }
     
     
 }
+
