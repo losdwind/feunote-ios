c3a457ca2d542535dd47a2a42853c62b078c1759
Aijie Shu <s524256521@gmail.com>
2022-06-17 09:26:36 +0800

    Change( completion handler n to async await)

--- Feunote/Data/Repository/AuthRepositoryImpl.swift
+++ Feunote/Data/Repository/AuthRepositoryImpl.swift
@@ -32,16 +32,48 @@ class AuthRepositoryImpl:AuthRepositoryProtocol {
         authService.configure()
     }
 
-    func signIn(username: String, password: String, completion: @escaping (Result<AuthStep, AuthError>) -> Void) {
-        authService.signIn(username: username, password: password, completion: completion)
+    func signIn(username: String, password: String) async throws -> AuthStep {
+        
+        return try await withCheckedThrowingContinuation({ continuation in
+            authService.signIn(username: username, password: password){
+                result in
+                switch result {
+                case .success(_):
+                    continuation.resume(returning: AuthStep.done)
+                case .failure(_):
+                    continuation.resume(throwing: AuthError)
+                }
+            }
+        })
     }
     
-    func signUp(username: String, email: String, password: String, completion: @escaping (Result<AuthStep, AuthError>) -> Void) {
-        authService.signUp(username: username, email: email, password: password, completion: completion)
+    func signUp(username: String, email: String, password: String, completion: @escaping (Result<AuthStep, AuthError>) -> Void) async await -> AuthStep {
+        return try await withCheckedThrowingContinuation({ continuation in
+            authService.signUp(username: username, email: email, password: password){
+                result in
+                switch result {
+                case .success(_):
+                    continuation.resume(returning: AuthStep.confirmSignUp)
+                case .failure(_):
+                    continuation.resume(throwing: AuthError)
+                }
+            }
+        })
+        
     }
     
-    func confirmSignUpAndSignIn(username: String, password: String, confirmationCode: String, completion: @escaping (Result<AuthStep, AuthError>) -> Void) {
-        authService.confirmSignUpAndSignIn(username: username, password: password, confirmationCode: confirmationCode, completion: completion)
+    func confirmSignUpAndSignIn(username: String, password: String, confirmationCode: String) -> Void) async throws -> AuthStep {
+        return try await withCheckedThrowingContinuation({ continuation in
+            authService.confirmSignUpAndSignIn(username: username, password: password, confirmationCode: confirmationCode){ result in
+                switch result {
+                case .success(_):
+                    continuation.resume(returning: AuthStep.signIn)
+                case .failure(_):
+                    continuation.resume(throwing: AuthError)
+                }
+            }
+        })
+        
     }
     
     
